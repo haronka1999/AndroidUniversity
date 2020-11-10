4b76f52f511df5e5d43d9564d0d0c5edda154ce7
Horvath Aron <haronka99@gmail.com>
2020-11-11 00:23:05 +0200

    animation doesnt go to main activirty

--- app/src/main/java/com/e/wheretoeat/splash/SplashActivity.kt
+++ app/src/main/java/com/e/wheretoeat/splash/SplashActivity.kt
@@ -1,32 +1,47 @@
 package com.e.wheretoeat.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
 import com.e.wheretoeat.R
+import com.e.wheretoeat.main.MainActivity
 
+
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var logo : ImageView
-    private lateinit var appName : ImageView
-    private lateinit var splashImg : ImageView
-    private lateinit var lottieAnimation : LottieAnimationView
+    private lateinit var appName: ImageView
+    private lateinit var splashImg: ImageView
+    private lateinit var lottieAnimation: LottieAnimationView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        logo = findViewById(R.id.logo);
-        appName = findViewById(R.id.appName);
-        splashImg = findViewById(R.id.splashImage);
-        lottieAnimation = findViewById(R.id.lottie);
 
-        splashImg.animate().translationY((-1600).toFloat()).setDuration(1000).startDelay = 4000
-        logo.animate().translationY((1400).toFloat()).setDuration(1000).startDelay = 4000
-        appName.animate().translationY((1400).toFloat()).setDuration(1000).startDelay = 4000
-        lottieAnimation.animate().translationY((1400).toFloat()).setDuration(1000).startDelay = 4000
+        appName = findViewById(R.id.appName)
+        splashImg = findViewById(R.id.splashImage)
+        lottieAnimation = findViewById(R.id.lottie)
+
+        splashImg.animate().translationY((-2000).toFloat()).setDuration(1000).startDelay = 4000
+        appName.animate().translationY((1800).toFloat()).setDuration(1000).startDelay = 4000
+        lottieAnimation.animate().translationY((1800).toFloat()).setDuration(1000).startDelay = 4000
+
+       // lottieAnimation.addAnimatorListener(Animator.AnimatorListener)
+        if ( ! lottieAnimation.isAnimating){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+//        Handler().postDelayed({
+//
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 1000)
+//
 
     }
 }
