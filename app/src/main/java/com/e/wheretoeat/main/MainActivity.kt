2222f1d47d72047eba1aaf82207054125e4113db
Horvath Aron <haronka99@gmail.com>
2020-11-14 19:35:59 +0200

    bottomnavigation corrected

--- app/src/main/java/com/e/wheretoeat/main/MainActivity.kt
+++ app/src/main/java/com/e/wheretoeat/main/MainActivity.kt
@@ -3,12 +3,19 @@ package com.e.wheretoeat.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.e.wheretoeat.R
+import com.e.wheretoeat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d("Helo", "in main activity")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = findNavController(R.id.myNavHostFragment)
+        val bottomNav = binding.bottomNavigationView
+        bottomNav?.setupWithNavController(navController)
     }
 }
