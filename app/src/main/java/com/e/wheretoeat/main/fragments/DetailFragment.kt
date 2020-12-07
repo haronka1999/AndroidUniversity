7b3e8ae337a5b0636f3b35ffb12aa87233739602
Horvath Aron <haronka99@gmail.com>
2020-12-07 16:49:32 +0200

    design added to the project and details fragment updated

--- app/src/main/java/com/e/wheretoeat/main/fragments/DetailFragment.kt
+++ app/src/main/java/com/e/wheretoeat/main/fragments/DetailFragment.kt
@@ -15,18 +15,23 @@ import com.e.wheretoeat.main.viewmodels.MainViewModel
 class DetailFragment : Fragment() {
 
     private val viewModel: MainViewModel by activityViewModels()
-    private lateinit var binding : FragmentDetailBinding
+    private lateinit var binding: FragmentDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         requireActivity().findViewById<View>(R.id.bottomNavigationView).visibility = View.VISIBLE
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
 
         binding.restNameTextView.text = viewModel.currentApiRestaurant.name
         binding.imageView.setImageResource(R.drawable.food)
-        binding.addressTextView.text = "Address: " +  viewModel.currentApiRestaurant.address
-        binding.priceTextView.text =  "Price: " + viewModel.currentApiRestaurant.price.toString()
+        val fullAddress =
+            "Address: ${viewModel.currentApiRestaurant.country}," +
+                    " ${viewModel.currentApiRestaurant.city},\n" +
+                    viewModel.currentApiRestaurant.address
+        val priceTag = "Price: ${viewModel.currentApiRestaurant.price}"
+        binding.addressTextView.text = fullAddress
+        binding.priceTextView.text = priceTag
 
         return binding.root
     }
