5473603652356c7f13dbd9e38b4c5545d6c4c0c6
Horvath Aron <haronka99@gmail.com>
2020-12-20 18:08:09 +0200

    favorite out it's done yeye

--- app/src/main/java/com/e/wheretoeat/main/fragments/DetailFragment.kt
+++ app/src/main/java/com/e/wheretoeat/main/fragments/DetailFragment.kt
@@ -12,47 +12,58 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.e.wheretoeat.R
 import com.e.wheretoeat.databinding.FragmentDetailBinding
+import com.e.wheretoeat.main.models.ApiRestaurant
 import com.e.wheretoeat.main.viewmodels.RestaurantViewModel
 import com.e.wheretoeat.main.viewmodels.MainViewModel
 
 
 class DetailFragment : Fragment() {
-    private lateinit var mRestViewModel: RestaurantViewModel
+
+
+    //viewmodels
     private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var mRestViewModel: RestaurantViewModel
+
     private lateinit var binding: FragmentDetailBinding
+    private lateinit var currentRestaurant: ApiRestaurant
+    private lateinit var favoriteRestaurants: MutableList<ApiRestaurant>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mRestViewModel = ViewModelProvider(this).get(RestaurantViewModel::class.java)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        currentRestaurant = mainViewModel.currentApiRestaurant
+        favoriteRestaurants = mainViewModel.favoriteRestaurants.value!!
+
         requireActivity().findViewById<View>(R.id.bottomNavigationView).visibility = View.VISIBLE
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
-        mRestViewModel = ViewModelProvider(this).get(RestaurantViewModel::class.java)
+
+        bindItems()
 
         //check if the current article is in the favorites
-
-
-        if (mainViewModel.favoriteRestaurants.value!!.contains(mainViewModel.currentApiRestaurant)) {
+        if (favoriteRestaurants.contains(currentRestaurant)) {
+            Log.d("Helo", "goToRed")
             binding.addToFav.setImageResource(R.drawable.ic_love_red)
         }
 
-        //set the attributes
-        val fullAddress =
-            "Address: ${mainViewModel.currentApiRestaurant.country}," +
-                    " ${mainViewModel.currentApiRestaurant.city},\n" +
-                    mainViewModel.currentApiRestaurant.address
-        val priceTag = "Price: ${mainViewModel.currentApiRestaurant.price}"
-
-        binding.restNameTextView.text = mainViewModel.currentApiRestaurant.name
-        binding.imageView.setImageResource(R.drawable.food)
-        binding.addressTextView.text = fullAddress
-        binding.priceTextView.text = priceTag
-
         binding.addToFav.setOnClickListener {
-            Log.d("Helo", "need to be red")
-            binding.addToFav.setImageResource(R.drawable.ic_love_red)
-            mainViewModel.favoriteRestaurants.value!!.add(mainViewModel.currentApiRestaurant)
-            //add an entity to the restaurant table
-            mRestViewModel.addRestaurant(mRestViewModel.castToEntityRestaurant(mainViewModel.currentApiRestaurant))
+            if(favoriteRestaurants.contains(currentRestaurant)){
+                Log.d("Helo", "clickedRestaurant contains ")
+                binding.addToFav.setImageResource(R.drawable.ic_love_normal)
+                mainViewModel.favoriteRestaurants.value!!.remove(currentRestaurant)
+            }else{
+                binding.addToFav.setImageResource(R.drawable.ic_love_red)
+                mainViewModel.favoriteRestaurants.value!!.add(currentRestaurant)
+
+                //add an entity to the restaurant table in ROOM database
+                mRestViewModel.addRestaurant(mRestViewModel.castToEntityRestaurant(currentRestaurant))
+            }
         }
 
         binding.showOnMap.setOnClickListener {
@@ -62,5 +73,14 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
+    private fun bindItems() {
+        val fullAddress =
+            "Address: ${currentRestaurant.country}," + " ${currentRestaurant.city},\n" + currentRestaurant.address
+        val priceTag = "Price: ${currentRestaurant.price}"
 
+        binding.restNameTextView.text = currentRestaurant.name
+        binding.imageView.setImageResource(R.drawable.food)
+        binding.addressTextView.text = fullAddress
+        binding.priceTextView.text = priceTag
+    }
 }
